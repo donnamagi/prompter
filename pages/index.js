a27f335f3d04f8eef81417df577286d6c8a97c59
Donna Mägi <magi.donna@gmail.com>
2024-01-04 18:41:19 +0200

    ref, marked library

--- pages/index.js
+++ pages/index.js
@@ -3,40 +3,50 @@ import { useState } from "react";
 import styles from "./index.module.css";
 import fillPlaceholders from "../utils/process";
 import { getTemplate, templates } from "../utils/get_template";
+import { marked } from 'marked';
+import { renderer } from '../utils/set_render';
+
+marked.use({ renderer });
 
 export default function Home() {
   const [result, setResult] = useState();
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    console.log('we here??')
-
-    const template = await getTemplate(event.target.value);
-    const result = await fillPlaceholders(template);
-
-    if (result === null) return; // user cancelled
-
+  async function callAPI(input) {
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ input: result }),
+        body: JSON.stringify({ input }),
       });
-
+  
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
-      var api_response = data.result[data.result.length - 1];
-      setResult(styleResponse(api_response.content));
-
+  
+      const api_response = data.result[data.result.length - 1];
+      return api_response.content;
+      
     } catch(error) {
       console.error(error);
       alert(error.message);
     }
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const template = await getTemplate(event.target.value);
+    const result = await fillPlaceholders(template);
+
+    if (result === null) return; // user cancelled
+
+    const api_response = await callAPI(result);
+    const html = marked.parse(api_response);
+
+    setResult(html);
   }
 
   return (
@@ -47,7 +57,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h3>Test</h3>
-        <div className={styles.result}>{result}</div>
+        <div  
+            className={styles.result} 
+            dangerouslySetInnerHTML={{ __html: result }} 
+        />
         <form onSubmit={onSubmit}>
           {Object.keys(templates).map((key) => (
             <input
@@ -62,73 +75,4 @@ export default function Home() {
       </main>
     </div>
   );
-}
-
-function styleResponse(response) {
-  const paragraphs = response.split('\n\n');
-  const styledParagraphs = [];
-
-  paragraphs.forEach((content, index) => {
-    const paragraph = (
-      <div key={index} id={index} className={styles.result} onClick={() => onSpecify(index, content)}>
-        <p>{content}</p>
-      </div>
-    );
-    styledParagraphs.push(paragraph);
-  });
-
-  return styledParagraphs;
-}
-
-function getComment(key, input) {
-  const userComment = prompt('What would you change?');
-  return `${key} --- "${input}" ${userComment}`;
-}
-
-async function onSpecify(key, input) {
-
-  const comment = getComment(key, input);
-
-  try {
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ input: comment }),
-    });
-
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw data.error || new Error(`Request failed with status ${response.status}`);
-    }
-
-    var api_response = data.result[data.result.length - 1];
-
-    const { id, content } = getKey(api_response.content);
-    replaceContent(id, content);
-    
-  } catch(error) {
-    console.error(error);
-    alert(error.message);
-  }
-}
-
-function getKey(syntax) {
-  const res = syntax.split('---');
-  if (res.length != 2) {
-    throw new Error(`Invalid syntax ${syntax}`);
-  }
-  const id = res[0].trim();
-  const content = res[1].trim();
-  return { id, content };
-}
-
-function replaceContent(id, content) {
-  try {
-    const div = document.getElementById(id);
-    div.firstChild.textContent = content;
-  } catch {
-    throw new Error(`Invalid id ${id}`);
-  }
 }
---
+++ utils/set_render.js
@@ -0,0 +1,29 @@
+import styles from '../pages/index.module.css';
+
+function generateIdAttribute() {
+  var id = Math.random().toString(36).substring(4, 10);
+  if (!isNaN(id.charAt(0))) {
+    // If it's a digit, replace the first character
+    id = 'd' + id.substring(1);
+  }
+  return id ? ` id="${id}"` : '';
+}
+  
+export const renderer = {
+  list(body, ordered) {
+    const type = ordered ? 'ol' : 'ul';
+    const id_att = generateIdAttribute();
+
+    return `<${type}${id_att} class=${styles.target}>\n${body}</${type}>\n`;
+  },
+
+  // tablerow(content) {
+  //   const id_att = generateIdAttribute();
+  //   return `<tr${id_att} class=${styles.target}>\n${content}</tr>\n`;
+  // },
+
+  paragraph(text) {
+    const id_att = generateIdAttribute();
+    return `<p${id_att} class=${styles.target}>${text}</p>\n`;
+  }
+};
