3773d9ad5f95a9c8199cc9837bde14caad4679cd
Donna Mägi <magi.donna@gmail.com>
2024-01-28 14:45:13 +0100

    handling diff notion formats

--- pages/api/notion/get_content.js
+++ pages/api/notion/get_content.js
@@ -3,19 +3,35 @@ import { Client } from "@notionhq/client";
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 export default async function (req, res) {
-
+  const { id } = req.body;
   const response = await notion.blocks.children.list({
-    block_id: req.body.id,
+    block_id: id,
     page_size: 50,
   });
 
-  var template_content = ''
+  let templateContent = '';
 
   for (const block of response.results) {
-    if (block.type === 'paragraph' && block.paragraph.rich_text[0]) {
-      template_content += block.paragraph.rich_text[0].plain_text;
+    switch (block.type) {
+      case 'heading_1':
+      case 'heading_2':
+      case 'heading_3':
+      case 'numbered_list_item':
+      case 'bulleted_list_item':
+        for (const object of block[block.type].rich_text) {
+          templateContent += `${object.plain_text}\n`;
+        }
+        break;
+      case 'paragraph':
+        for (const object of block.paragraph.rich_text) {
+          templateContent += `${object.plain_text}\n`;
+        }
+        if (block.paragraph.rich_text.length === 0) {
+          templateContent += '\n';
+        }
+        break;
     }
   }
 
-  res.status(200).json(template_content);
+  res.status(200).json(templateContent);
 }
