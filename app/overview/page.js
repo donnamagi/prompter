8c47acd53a658f299ea0ea207d39ff0065d1d274
Donna Mägi <magi.donna@gmail.com>
2024-02-05 23:28:36 +0100

    fix 2.0

--- app/overview/page.js
+++ app/overview/page.js
@@ -1,10 +1,8 @@
 "use client"
 
-import { React, useContext } from 'react';
-import { StateContext } from 'app/state-provider';
+import { React } from 'react';
 import Overview from "@/components/Overview";
 
 export default function Page() {
-  const {template} = useContext(StateContext);
-  return <Overview template={template} />
+  return <Overview />
 }
--- components/Overview.js
+++ components/Overview.js
@@ -5,13 +5,18 @@ import { Textarea } from "@/ui/textarea"
 import { Button } from "@/ui/button"
 import { PaperPlaneIcon, ResetIcon } from "@radix-ui/react-icons"
 import Link from 'next/link'
+import { useContext } from 'react'
+import { StateContext } from 'app/state-provider'
 
-export default function Overview(props) {
+export default function Overview() {
+  const {template} = useContext(StateContext);
+  if (!template) return null;
+
   return (
     <div className='fixed top-1/3 w-2/3 lg:w-1/3'>
       <div className="grid w-full gap-1.5">
-        <Label htmlFor="message-2" className='dark:text-zinc-300'>{props.template.title}</Label>
-        <Textarea placeholder="Type your message here." id="message-2" defaultValue={props.template.content} className='min-h-[300px]' />
+        <Label htmlFor="message-2" className='dark:text-zinc-300'>{template.title}</Label>
+        <Textarea placeholder="Type your message here." id="message-2" defaultValue={template.content} className='min-h-[300px]' />
         <p className="text-sm text-muted-foreground">
           This is the prompt you're forwarding to OpenAI.
         </p>
